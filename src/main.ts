ad84f699214c2c0c923ea9ebeef8a1584dc698df
Michael Young <micke.young@gmail.com>
2019-03-01 16:08:16 +0100

    Added charting lib, example page and bootstrap-grid

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import 'hammerjs';
+import 'chart.js';
+
 
 if (environment.production) {
   enableProdMode();
