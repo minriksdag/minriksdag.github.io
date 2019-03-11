0e9a608668a1b8a76267a876bc4bd85e7e14171a
Michael Young <micke.young@gmail.com>
2019-03-11 11:05:07 +0100

    Made graph empty on load

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -10,13 +10,12 @@ import { Color, BaseChartDirective, Label } from 'ng2-charts';
 })
 export class CompareComponent implements OnInit, OnChanges {
   public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    { data: [], label: '' }
   ];
   public lineChartLabels: Label[];
   public lineChartOptions: ChartOptions = {
     responsive: true,
+    maintainAspectRatio: false,
     scales: {
       yAxes: [
         {
@@ -53,7 +52,6 @@ export class CompareComponent implements OnInit, OnChanges {
     }
   ];
   public lineChartLegend = true;
-  public lineChartType = 'line';
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective;
 
