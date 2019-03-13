634befd3a2e608d64e270313d3dd85cee9666a96
Patrik Sylve <lan15psy@student.lu.se>
2019-03-13 10:06:11 +0100

    la

--- src/app/riksdag/riksdag.component.ts
+++ src/app/riksdag/riksdag.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {default as members} from './members.json';
+import {default as members} from '../../assets/members.json';
 
 export interface Party {
   id: number;
@@ -20,18 +20,16 @@ export class RiksdagComponent implements OnInit {
     parties: Party[] =[
       { id: 1, name: 'V' },
       { id: 2, name: 'S' },
-      { id: 3, name: 'Mp' },
+      { id: 3, name: 'MP' },
       { id: 4, name: 'C' },
       { id: 5, name: 'L' },
       { id: 6, name: 'M' },
-      { id: 7, name: 'Kd' },
-      { id: 8, name: 'Sd' }
+      { id: 7, name: 'KD' },
+      { id: 8, name: 'SD' }
     ];
 
     members: [];
-    stats: [{ja: 0, nej:0, frånvaro: 0, avstår: 0}];
-
-
+    stats = {ja: 0, nej:0, frånvaro: 0, avstår: 0};
 
     selectedParty = '';
     memberParty = '';
@@ -42,21 +40,22 @@ export class RiksdagComponent implements OnInit {
         let no, yes, abs, blank = 0;
         let id = Object.keys(members).filter(memberID=> (members[memberID].party === party.name));
         let list = [];
-        for(var i = 0; i < id.length; i++) {
+        for(var i = 0; i < 10; i++) {
             let member = members[id[i]];
 
             yes += member.aggregate_votes.Ja;
             no += member.aggregate_votes.Nej;
             abs += member.aggregate_votes.Frånvarande;
-            blank += ember.aggregate_votes.Avstår;
+            blank += member.aggregate_votes.Avstår;
             list.push(member);
         }
          this.members = list;
-         this.stats.ja = yes; 
+         this.stats.ja = yes;
          console.log(yes);
 
 
     }
+
 
 
   constructor() { }
